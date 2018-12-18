335e3a5eccdfa411d0821b5c0794b88f50b8b5e3
Elias Albert <sailetrebla@gmail.com>
2018-12-18 14:11:03 -0300

    Adicionando atualizações

--- Trabalho de ED/src/tabalhoinsertionsort/InsertinSorttest.java
+++ Trabalho de ED/src/tabalhoinsertionsort/InsertinSorttest.java
@@ -1,27 +1,31 @@
 package tabalhoinsertionsort;
 
 import java.util.Arrays;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 public class InsertinSorttest {
 	public static void main(String[] args) {
+
 		  int i,j;
 		  String S;
-		  String[]v= {"Aa","Ac","Af","An","Az","Aj","Bc","Bd","Ba"};
-		  System.out.println(Arrays.toString(v));
+		  String[]v= {"na","aC","Af","Ac","Ef"};
 		  
-		  for (j = 1; j < v.length; j++) { 
+	  for (j = 1; j < v.length; j++) { 
 		    S = v[j];
 		    i = j - 1;
-		    while (i >= 0) {
-		      if (S.compareTo(v[i]) > 0) {
+	    while (i >= 0) {
+		      if (S.toLowerCase().compareTo(v[i].toLowerCase()) > 0) {
 		        break;
-		      }
+	      }
 		      v[i + 1] = v[i];
-		      i--;
+	      i--;
 		   }
-		    v[i + 1] = S;
+	    v[i + 1] = S;
 		    System.out.println(Arrays.toString(v));
 		  }
 		  System.out.println("InsertSort---" +Arrays.toString(v));
+
 	}
 }
